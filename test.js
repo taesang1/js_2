7eb65a4ff03a6e2fa325b77159dba3d6716cdf05
taesang <tsjung@intellisys.co.kr>
2022-07-06 17:55:10 +0900

    이미지 삭제, 테두리 추가하여 전체영역 확인

--- test.js
+++ test.js
@@ -1,33 +1,17 @@
-console.log('lode')
 let tagArea = document.querySelector('#main');
 console.log(tagArea)
 let box = document.createElement('div');
 let title = document.createElement('p');
-let img_grid = document.createElement('ui')
 
 box.setAttribute('class', 'intellisys');
 box.style.width = '100%'
-box.style.height = '300px'
+box.style.height = '200px'
+box.style.border = '1px solid black'
 
 title.innerHTML = '추천상품'
 title.style.textAlign = 'center'
 title.appendChild(document.createElement('hr'))
 
-img_grid.style.display ='flex'
-img_grid.style.width = '100%'
-img_grid.style.height = '90%'
-for (var i = 0; i < 5; i++) {
-    imgs = document.createElement('li')
-    imgs.style.listStyle = 'none'
-    a = document.createElement('a')
-    img = document.createElement('img')
-    img.setAttribute('src','images/icon/ico-company03.png')
-    a.appendChild(img)
-    imgs.appendChild(a)
-    img_grid.appendChild(imgs)
-}
-
 box.appendChild(title)
-box.appendChild(img_grid)
 
 tagArea.appendChild(box);
