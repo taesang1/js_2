857c3b3233fe3aab2cc0d4d9b7d1e65c94c2f10b
taesang1 <94157053+taesang1@users.noreply.github.com>
2022-07-06 18:19:42 +0900

    Create ts.js

---
+++ ts.js
@@ -0,0 +1,17 @@
+let tagArea = document.querySelector('#prdDetail');
+console.log(tagArea)
+let box = document.createElement('div');
+let title = document.createElement('p');
+
+box.setAttribute('class', 'intellisys');
+box.style.width = '100%'
+box.style.height = '200px'
+box.style.border = '1px solid black'
+
+title.innerHTML = '추천상품'
+title.style.textAlign = 'center'
+title.appendChild(document.createElement('hr'))
+
+box.appendChild(title)
+
+tagArea.appendChild(box);
