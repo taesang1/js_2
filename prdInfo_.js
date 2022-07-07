1017b4f8977ae09410e136682358fb6a0854ec32
taesang1 <94157053+taesang1@users.noreply.github.com>
2022-07-07 14:32:25 +0900

    Update prdInfo_.js

--- prdInfo_.js
+++ prdInfo_.js
@@ -8,7 +8,7 @@ box_prdInfo.style.width = '100%'
 box_prdInfo.style.height = '200px'
 box_prdInfo.style.border = '1px solid black'
 
-title_prdInfo.innerHTML = 'prdInfo_추천상품'
+title_prdInfo.innerHTML = '아이템 설명 추천상품'
 title_prdInfo.style.textAlign = 'center'
 title_prdInfo.appendChild(document.createElement('hr'))
 
