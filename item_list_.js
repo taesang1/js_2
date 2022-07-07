09f05133a8b3882e75b576c4b15d4b1919e88ff5
taesang1 <94157053+taesang1@users.noreply.github.com>
2022-07-07 14:17:14 +0900

    Update and rename item_list.js to item_list_.js

--- item_list_.js
+++ item_list_.js
@@ -1,5 +1,5 @@
 let tagArea_item_list = document.querySelector('.xans-product-listnormal');
-console.log(tagArea)
+console.log(tagArea_item_list)
 let box_item_list = document.createElement('div');
 let title_item_list = document.createElement('p');
 
@@ -8,7 +8,7 @@ box_item_list.style.width = '100%'
 box_item_list.style.height = '200px'
 box_item_list.style.border = '1px solid black'
 
-title_item_list.innerHTML = 'xans-order-basketpackage_추천상품'
+title_item_list.innerHTML = 'xans-product-listnormal_추천상품'
 title_item_list.style.textAlign = 'center'
 title_item_list.appendChild(document.createElement('hr'))
 
