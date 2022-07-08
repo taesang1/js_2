1f7a4ff5606a308657f54ed49f63b22e1f654193
taesang1 <94157053+taesang1@users.noreply.github.com>
2022-07-08 11:56:39 +0900

    Update cart_row.js

--- cart_row.js
+++ cart_row.js
@@ -12,9 +12,9 @@ title_row.innerHTML = '장바구니상품 추천상품'
 title_row.style.textAlign = 'center'
 title_row.appendChild(document.createElement('hr'))
 box_row.appendChild(title_row)
-for (var i = 1; i < tagArea_row.length; i++) {
+for (var i = 0; i < tagArea_row.length; i++) {
     tagArea_ = tagArea_row[i].childNodes
-    for (var j = 1; j < tagArea_.length; j++) {
+    for (var j = 0; j < tagArea_.length; j++) {
         if (tagArea_[j].className != null  && tagArea_[j].className.includes('xans-record-')){
             tagArea_[j].after(box_row);
         }
