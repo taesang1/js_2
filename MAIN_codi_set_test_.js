cb4623735fc7b7d6275652c1c19436e48eda6551
taesang1 <94157053+taesang1@users.noreply.github.com>
2022-08-29 19:01:01 +0900

    Create MAIN_codi_set_test_.js

---
+++ MAIN_codi_set_test_.js
@@ -0,0 +1,130 @@
+class main_code_set {
+  constructor(target) {
+    for (const i in target) {
+      this.tagArea = document.querySelector(`#${target[i]}`);
+      if (this.tagArea == null) continue
+      this.set_box()
+    }
+  }
+
+  info() {
+    this.title = document.createElement("p");
+    this.intellisys_box = document.createElement("div");
+    this.box = document.createElement("div");
+    this.slide_wrapper = document.createElement("div");
+  }
+
+  set_up() {
+    this.img = document.createElement("img");
+    this.img.style.width = "100%";
+    this.img.style.height = "180px";
+    this.link = document.createElement("a");
+    this.row = document.createElement("li");
+    this.row.style.width = '100%'
+    this.row.style.listStyle = 'none'
+    this.code_title = document.createElement("p");
+    this.code_title.style.textAlign='left';
+    this.code_title.style.position='absolute'
+  }
+
+  set_up2(text){
+    this.slides = document.createElement("ul");
+    this.slides.setAttribute("class", text);
+    this.slides.style.display = 'flex'
+    this.hr = document.createElement("hr")
+  }
+
+  set_code(res) {
+    res = JSON.parse(res)
+    for (const i in res) {
+      this.set_up2(i)
+      for (const j in res[i]){
+        this.set_up()
+        if (j.includes('title')){
+          this.code_title.innerHTML = res[i][j]
+          this.slide_wrapper.appendChild(this.code_title);
+        }
+        if (res[i][j]['imgurl'] === 0 || res[i][j]['imgurl'] == null) continue
+        this.link.setAttribute("href", res[i][j]['link']);
+        this.img.style.objectFit = 'contain'
+        this.img.setAttribute("src", res[i][j]['imgurl']);
+        this.link.appendChild(this.img);
+        this.row.appendChild(this.link);
+        this.slides.appendChild(this.row);
+      }
+      this.slide_wrapper.appendChild(this.slides)
+      this.slide_wrapper.appendChild(this.hr)
+    }
+    this.loading(false)
+  }
+
+  set_attrs() {
+    this.slide_wrapper.setAttribute("class", "slide_wrapper");
+
+    this.box.style.width = "100%"
+    this.box.style.height = "max-content"
+    this.box.style.margin = 'auto'
+    this.box.setAttribute("class", "box");
+
+    this.intellisys_box.setAttribute("class", "intellisys");
+    this.intellisys_box.style.display = "flex";
+
+    this.title.innerHTML = "코디상품";
+    this.title.style.textAlign = "center";
+    this.title.appendChild(document.createElement("hr"));
+  }
+
+  set_box() {
+    this.info()
+    this.set_attrs()
+    
+    this.box.appendChild(this.title);
+    this.loading(true)
+    ref()
+    this.box.appendChild(this.slide_wrapper)
+
+    this.tagArea.appendChild(this.box)
+  }
+
+  loading(status){
+    if (status){
+      var loading_img = document.createElement("img")
+      loading_img.setAttribute("src",'https://t1.daumcdn.net/cfile/tistory/233F6D505786DA870A');
+      loading_img.setAttribute("id", 'loading');
+      this.box.appendChild(loading_img)
+      this.box.style.textAlign='center'
+    } else {
+      var loading_img = document.querySelector("#loading");
+      this.box.removeChild(loading_img)
+      this.box.style.textAlign=''
+    }
+  }
+}
+
+main_code_set_ = new main_code_set(['productList', 'main'])
+
+function ref() {
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "http://127.0.0.1:8000";
+
+  // 요청을 초기화 합니다.
+  xhr.open(method, url);
+
+  // onreadystatechange 이벤트를 이용해 요청에 대한 응답 결과를 처리합니다.
+  xhr.onreadystatechange = function (event) {
+    const { target } = event;
+
+    if (target.readyState === XMLHttpRequest.DONE) {
+      const { status } = target;
+
+      if (status === 0 || (status >= 200 && status < 400)) {
+        main_code_set_.set_code(xhr.response)
+      } else {
+        console.log('Error', xhr.status, xhr.statusText)
+      }
+    }
+  };
+  // 서버에 요청을 보냅니다.
+  xhr.send();
+}
